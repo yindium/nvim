b55f844ee3877e31026249824670d12ad575c566
inharul <inharulstylo@gmail.com>
2023-05-07 18:57:56 +0530

    add: lualine-icons

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,12 +6,19 @@ end
 
 vim.cmd [[packadd packer.nvim]]
 
+-- PackerCompile - regenerate compiled loader file
+-- PackerInstall - clean and then install the missing plugins
+-- PackerClean - clean the unused plugins
+-- PackerStatus - lists installed plugins
+-- PackerUpdate - updates plugins (!risky if commits have errors)
+-- PackerLoad {plugin} - loads the plugin immediately
+
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
-  use {
-    'svrana/neosolarized.nvim',
-    requires = { 'tjdevries/colorbuddy.nvim' }
-  }
   use 'jiangmiao/auto-pairs'
-  use 'nvim-lualine/lualine.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons' }
+  }
 end)
