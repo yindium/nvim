06be7fbee0c3b3af7959a2464f27dec73f90f332
yindium <inharulchu6@gmail.com>
2023-05-19 09:06:50 +0530

    upd: bufferline

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -1,2 +1,21 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+local bufferline = require 'bufferline'
+
+bufferline.setup{
+  options = {
+    mode = 'tabs',
+    -- color_icons = true,
+    style_preset = bufferline.style_preset.no_italic,
+    -- show_buffer_close_icons = true,
+    show_close_icon = false,
+    show_tab_indicators = false,
+    offsets = {
+      {
+        filetype = 'NvimTree',
+        text = ' 󰉋 '  ,
+        text_align = 'right',
+        seperator = true,
+      }
+    },
+  }
+}
