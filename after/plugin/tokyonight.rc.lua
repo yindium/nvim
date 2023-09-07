351a8e4ff94c3c9bc5f4f90f111e6a6a1d7787c2
Yazya Indium <inharulchu6@gmail.com>
2023-09-07 19:25:02 +0530

    theme: transparent

--- after/plugin/tokyonight.rc.lua
+++ after/plugin/tokyonight.rc.lua
@@ -1,1 +1,11 @@
-vim.cmd[[colorscheme tokyonight-night]]
+require("tokyonight").setup({
+  style = "night",
+  transparent = true,
+  terminal_colors = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  }
+})
+
+vim.cmd [[colorscheme tokyonight]]
